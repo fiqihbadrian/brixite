9ec2805d9ec2ea1a3c7463c0970236a90c55b2ee
bdr.de <76670473+fiqihbadrian@users.noreply.github.com>
2025-04-14 11:33:07 +0700

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -1,56 +1,26 @@
 export const products = [
   {
     id: 1,
-    name: 'T-Shirt Basic',
-    category: 'T-Shirts',
-    price: 199000,
-    image: '/product/org1.png',
-    description: 'Basic comfortable t-shirt with premium quality material',
-    link: 'https://wa.me/6287875150524'
-  },
-  {
-    id: 2,
-    name: 'Hoodie Premium',
-    category: 'Hoodies',
-    price: 399000,
-    image: '/product/org2.png',
-    description: 'Warm and stylish hoodie for everyday wear',
-    link: 'https://wa.me/6287875150524'
-  },
-  {
-    id: 3,
-    name: 'Pants Street',
-    category: 'Sweater',
+    name: 'Hoodie Dope - Brixite Original',
+    category: 'Hoodie',
     price: 299000,
-    image: '/product/org3.png',
-    description: 'Trendy streetwear pants with comfortable fit',
-    link: 'https://wa.me/6287875150524'
-  },
-  {
-    id: 4,
-    name: 'Pants Street',
-    category: 'Sweater',
-    price: 299000,
-    image: '/product/org3.png',
-    description: 'Trendy streetwear pants with comfortable fit',
-    link: 'https://wa.me/6287875150524'
-  },
-  {
-    id: 5,
-    name: 'Pants Street',
-    category: 'Sweater',
-    price: 299000,
-    image: '/product/org3.png',
-    description: 'Trendy streetwear pants with comfortable fit',
-    link: 'https://wa.me/6287875150524'
-  },
-  {
-    id: 6,
-    name: 'T-Shirt Basic',
-    category: 'T-Shirts',
-    price: 199000,
-    image: '/product/org1.png',
-    description: 'Basic comfortable t-shirt with premium quality material',
+    imageThumbnail: '/product/org1.png',
+    images: [
+      '/product/org1.png',
+      '/product/org3.png'
+    ],
+    description: [
+      'Trendy hoodie with comfortable fit',
+      'Premium quality material with comfortable cotton blend',
+      'Available in multiple colors: black, navy, gray',
+      '\n',
+      'Features:',
+      '- Adjustable drawstrings',
+      '- Kangaroo pocket',
+      '- Ribbed cuffs and hem'
+    ],
+    colors: ['Black', 'white'],
+    sizes: ['M', 'L'],
     link: 'https://wa.me/6287875150524'
   },
 
