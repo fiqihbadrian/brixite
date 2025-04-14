79806b10d667d83636c8e1cf53e330d4885a4a06
bdr.de <76670473+fiqihbadrian@users.noreply.github.com>
2025-04-14 15:02:01 +0700

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -23,6 +23,29 @@ export const products = [
     sizes: ['M', 'L'],
     link: 'https://wa.me/6287875150524'
   },
+  {
+    id: 2,
+    name: 'Sweater Dope - Brixite Original',
+    category: 'Sweater',
+    price: 280000,
+    imageThumbnail: '/product/org2.png',
+    images: [
+      '/product/org2.png'
+    ],
+    description: [
+      'Trendy hoodie with comfortable fit',
+      'Premium quality material with comfortable cotton blend',
+      'Available in multiple colors: black, navy, gray',
+      '\n',
+      'Features:',
+      '- Adjustable drawstrings',
+      '- Kangaroo pocket',
+      '- Ribbed cuffs and hem'
+    ],
+    colors: ['Black', 'white'],
+    sizes: ['M', 'L'],
+    link: 'https://wa.me/6287875150524'
+  },
 
   // Add more products as needed
 ];
