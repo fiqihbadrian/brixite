2e4970c444210d01f6a60ad513cc5a7b26b6c384
bdr.de <76670473+fiqihbadrian@users.noreply.github.com>
2025-04-17 06:29:30 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import './globals.css'
+import './globals.css';
 import Script from 'next/script';
 
 export const metadata = {
@@ -24,7 +24,7 @@ export const metadata = {
     description: 'Brixite Society, is all about bringing bold, stylish streetwear that speaks to your authentic vibe. We\'re here to make your everyday look stand out, with comfy fits and eye-catching designs. Join us, make a statement, and let\'s keep it cool and fresh!',
     images: ['./image.png'],
   },
-}
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -32,23 +32,27 @@ export default function RootLayout({ children }) {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<Script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-P69WVX4N');</Script>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-P69WVX4N');`}
+        </Script>
       </head>
       <body>
-      <noscript>
+        {/* Google Tag Manager (noscript) - letakkan sedekat mungkin setelah tag <body> */}
+        <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-P69WVX4N"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
           ></iframe>
-      </noscript>
-      {children}
+        </noscript>
+        {children}
       </body>
     </html>
-  )
+  );
 }
